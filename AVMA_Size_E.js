f8bf70f8520d0e6b7eadf6bf12b7265751777b54
ThincMoLab <yuedu@tamu.edu>
2026-02-13 17:49:54 -0600

    Update AVMA_Size_E.js

--- AVMA_Size_E.js
+++ AVMA_Size_E.js
@@ -589,7 +589,7 @@ num_trials_sound_check = 20;
     texRes : 128, interpolate : true, depth : -3.0 
   });
   
-  console.log('mark1')
+  console.log('mark2')
   RT_Stim_Hand = new visual.Polygon ({
     win: psychoJS.window, name: 'RT_Stim_Hand', units : 'height', 
     edges: 32, size:[0.025, 0.025],
